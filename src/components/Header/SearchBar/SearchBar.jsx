1f55df2eba78652101d6229475f7b0cc289972f0
matthewjchurch <matthewjchurch@gmail.com>
2020-11-09 12:15:03 +0000

    now handles backspace

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -3,18 +3,30 @@ import FilterList from '../FilterList/FilterList';
 
 const SearchBar = (props) => {
     const {queryStr, setQueryStr, setBeers} = props;
+
     const searchHandler = e => {
+        let allowForSpaces = e.target.value.replace(" ", "_")
         e.target.value === "" ?
         setBeers([]):
         queryStr.match(/beer_name/) ?
-        setQueryStr(queryStr.replace(/beer_name=\w+&/, `beer_name=${e.target.value}&`)) :
-        setQueryStr(`${queryStr}beer_name=${e.target.value}&`)
+        setQueryStr(queryStr.replace(/beer_name=\w+&/, `beer_name=${allowForSpaces}&`)) :
+        setQueryStr(`${queryStr}beer_name=${allowForSpaces}&`)
         setBeers(queryStr);
+    }
+
+    const backspaceHandler = (e) => {
+        if (e.keyCode === 8) {
+            searchHandler();
+        }
     }
     return (
         <div>
             <label htmlFor="name">Know what you're looking for?</label>
-            <input onChange={searchHandler} type="text"/>
+            <input 
+                onChange={searchHandler} 
+                onKeydown={backspaceHandler} 
+                type="text"
+            />
         </div>
     )
 }
